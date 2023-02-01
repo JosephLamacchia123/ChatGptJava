cd57b116817efb6fb2cc2d97f5d8a352d2d89910
Joe La Macchia <josephlamacchia1@gmail.com>
2023-02-01 00:37:13 -0500

    Adding service to generate cover letters.
    Updating pojos to use Lombok to make more readable

--- BaseProject/src/main/java/Application.java
+++ BaseProject/src/main/java/Application.java
@@ -1,7 +1,6 @@
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) throws UnirestException, IOException {
--- BaseProject/src/main/java/CoverLetterResponse.java
+++ BaseProject/src/main/java/CoverLetterResponse.java
@@ -1,5 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class CoverLetterResponse {
 
     private long created;
@@ -9,122 +11,22 @@ public class CoverLetterResponse {
     private Choice[] choices;
     private String object;
 
-    public long getCreated() {
-        return created;
-    }
 
-    public void setCreated(long created) {
-        this.created = created;
-    }
 
-    public Usage getUsage() {
-        return usage;
-    }
-
-    public void setUsage(Usage usage) {
-        this.usage = usage;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Choice[] getChoices() {
-        return choices;
-    }
-
-    public void setChoices(Choice[] choices) {
-        this.choices = choices;
-    }
-
-    public String getObject() {
-        return object;
-    }
-
-    public void setObject(String object) {
-        this.object = object;
-    }
-
+    @Data
     public static class Usage {
         private int completion_tokens;
         private int prompt_tokens;
         private int total_tokens;
-
-        public int getCompletion_tokens() {
-            return completion_tokens;
-        }
-
-        public void setCompletion_tokens(int completion_tokens) {
-            this.completion_tokens = completion_tokens;
-        }
-
-        public int getPrompt_tokens() {
-            return prompt_tokens;
-        }
-
-        public void setPrompt_tokens(int prompt_tokens) {
-            this.prompt_tokens = prompt_tokens;
-        }
-
-        public int getTotal_tokens() {
-            return total_tokens;
-        }
-
-        public void setTotal_tokens(int total_tokens) {
-            this.total_tokens = total_tokens;
-        }
     }
 
+    @Data
     public static class Choice {
         private String finish_reason;
         private int index;
         private String text;
         @JsonProperty("logprobs")
         private Object logProbs;
-
-        public String getFinish_reason() {
-            return finish_reason;
-        }
-
-        public void setFinish_reason(String finish_reason) {
-            this.finish_reason = finish_reason;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
-        }
-
-        public Object getLogProbs() {
-            return logProbs;
-        }
-
-        public void setLogProbs(Object logProbs) {
-            this.logProbs = logProbs;
-        }
     }
 
 }
